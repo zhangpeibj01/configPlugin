ceb8e7d7bd8e79eddcd29b2c2447be6b29ff0956
zhangpeibj01 <zhangpeibj01@yuanfudao.com>
2022-06-10 10:36:45 +0800

    feat: format

--- Sources/tuist-config/main.swift
+++ Sources/tuist-config/main.swift
@@ -3,16 +3,10 @@ import ArgumentParser
 
 let path = FileManager.default.currentDirectoryPath + "/Tuist/config.json"
 let pathURL = URL(fileURLWithPath: path)
-let data = try? Data(contentsOf: pathURL)
+var data: Data? { try? Data(contentsOf: pathURL) }
 
 
 struct ConfigOptions: ParsableArguments {
-
-    @Flag(help: "Show config.json content")
-    var show = false
-
-    @Flag(help: "Clear config.json content")
-    var clear = false
 
     @Argument(help: "The key")
     var key: String?
@@ -23,17 +17,14 @@ struct ConfigOptions: ParsableArguments {
     @Option(help: "remove key-value from config.json")
     var remove: String?
 
+    @Flag(help: "help")
+    var aid = false
+
+    @Flag(help: "Clear config.json content")
+    var clear = false
 }
 
 let options = ConfigOptions.parseOrExit()
-
-if options.show {
-    if let data = data, let convertedString = String(data: data, encoding: String.Encoding.utf8) {
-        print(convertedString)
-    } else {
-        print("no config file!")
-    }
-}
 
 if options.clear {
     if FileManager.default.fileExists(atPath: pathURL.path) {
@@ -136,7 +127,73 @@ if
     }
 }
 
+if options.aid {
+    print("SUBCOMMANDS")
+    print("\("key".padding(toLength: 20, withPad: " ", startingAt: 0))the config key")
+    print("\("value".padding(toLength: 20, withPad: " ", startingAt: 0))the config value")
+    print("\("remove".padding(toLength: 20, withPad: " ", startingAt: 0))remove key-value from config")
+    print("\("clear".padding(toLength: 20, withPad: " ", startingAt: 0))clear all configs")
+}
+
 let cachePath = NSHomeDirectory().appending("/.tuist/Cache/Manifests")
 if FileManager.default.fileExists(atPath: cachePath) {
     try FileManager.default.removeItem(atPath: cachePath)
+}
+
+if
+    !options.aid,
+    let data = data,
+    let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
+
+    print("--------------------------current supported config--------------------------")
+    let supportedKeys = ["key", "mockAllModules", "focusModules", "mockModules", "integrateSwiftLint", "uploadBuildLog", "keepAllTargets", "previewMode", "enableRemoteCache", "remoteCacheProducer", "remotePreviewResumeCacheProducer"]
+    let keyNumberMaxCount = (supportedKeys.map { $0.count }.max() ?? 0) + 4
+    let realSupportedValues = supportedKeys.map { (value) -> String in
+        if let currentValue = json[value] {
+            if type(of: currentValue) == type(of: NSNumber(value: true)) {
+                return "true"
+            } else if type(of: currentValue) == type(of: NSNumber(value: false)) {
+                return "false"
+            } else if let array = currentValue as? [String] {
+                return array.joined(separator: ",")
+            } else {
+                return "\(currentValue)"
+            }
+        } else {
+            return "<null>"
+        }
+    }
+    var supportedValues = ["value"]
+    supportedValues.append(contentsOf: realSupportedValues)
+    let valueNumberMaxCount = (supportedValues.map { $0.count }.max() ?? 0) + 4
+    let defaultedValues = ["defaultValue", "false", "[]", "[]", "false", "false", "true", "false", "false", "false", "false"]
+    let defaultedValueMaxCount = (defaultedValues.map { $0.count }.max() ?? 0)
+    supportedKeys.enumerated().forEach { (index, key) in
+        print("|\(key.padding(toLength: keyNumberMaxCount, withPad: " ", startingAt: 0))|\(supportedValues[index].padding(toLength: valueNumberMaxCount, withPad: " ", startingAt: 0))|\(defaultedValues[index].padding(toLength: defaultedValueMaxCount, withPad: " ", startingAt: 0))")
+    }
+    print("--------------------------current not supported config--------------------------")
+    var notSupportedKeys = ["key"]
+    var notSupportedValues = ["value"]
+    json.forEach { (key, value) in
+        if !supportedKeys.contains(key) {
+            notSupportedKeys.append(key)
+            let newValue = { () -> String in
+                if type(of: value) == type(of: NSNumber(value: true)) {
+                    return "true"
+                } else if type(of: value) == type(of: NSNumber(value: false)) {
+                    return "false"
+                } else if let array = value as? [String] {
+                    return array.joined(separator: ",")
+                } else {
+                    return "\(value)"
+                }
+            }()
+            notSupportedValues.append(newValue)
+        }
+    }
+    let notSupportedKeyNumberMaxCount = (notSupportedKeys.map { $0.count }.max() ?? 0) + 4
+    let notSupportedValueNumberMaxCount = (notSupportedValues.map { $0.count }.max() ?? 0) + 4
+    notSupportedKeys.enumerated().forEach { (index, key) in
+        print("|\(key.padding(toLength: notSupportedKeyNumberMaxCount, withPad: " ", startingAt: 0))|\(notSupportedValues[index].padding(toLength: notSupportedValueNumberMaxCount, withPad: " ", startingAt: 0))")
+    }
 }
